4895c4d4c4d975acdff158994ea5799d75c6f08b
John Marsing <JohnMarsing@outlook.com>
2024-07-25 22:47:41 -0700

    Enhanced BookChapter

---
+++ MyHebrewBible/MyHebrewBible.Client/Components/BookChapterToolbar/ButtonsToShow.cs
@@ -0,0 +1,8 @@
+﻿namespace MyHebrewBible.Client.Components.BookChapterToolbar;
+
+public enum ButtonsToShow
+{
+	Both = 1,
+	PreviousOnly = 2,
+	NextOnly = 3
+}
--- MyHebrewBible/MyHebrewBible/Database/Queries/BookChapter.sql
+++ MyHebrewBible/MyHebrewBible/Database/Queries/BookChapter.sql
@@ -1,4 +1,4 @@
-﻿		SELECT ID, BCV, Verse, VerseOffset, KJV, DescH, DescD  
-		FROM Scripture 
-		WHERE BookID=1 and Chapter=2
-		ORDER BY ID
+﻿SELECT ID, BCV, Verse, VerseOffset, KJV, DescH, DescD  
+FROM Scripture 
+WHERE BookID=1 and Chapter=2
+ORDER BY ID
