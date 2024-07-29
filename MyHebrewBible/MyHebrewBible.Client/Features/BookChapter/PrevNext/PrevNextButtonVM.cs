6ed2caacfbbfd8c1dbe764bb12478d706d20c5d6
John Marsing <JohnMarsing@outlook.com>
2024-07-29 12:14:06 -0700

    heavy refactor of book chapter more to come

---
+++ MyHebrewBible/MyHebrewBible.Client/Features/BookChapter/PrevNext/PrevNextButtonVM.cs
@@ -0,0 +1,83 @@
+﻿using MyHebrewBible.Client.Enums;
+using MyHebrewBible.Client.Helpers;
+
+namespace MyHebrewBible.Client.Features.BookChapter.PrevNext;
+
+public class PrevNextButtonVM
+{
+	public BookChapterPrevNext? PrevBibleBook { get; set; }
+	public BookChapterPrevNext? NextBibleBook { get; set; }
+
+	public PrevNextButtonVM(BookAndChapter bookAndChapter)
+	{
+		PrevBibleBook = SetPrevBookChapter(bookAndChapter);
+		NextBibleBook = SetNextBookChapter(bookAndChapter);
+	}
+
+	private BookChapterPrevNext SetPrevBookChapter(BookAndChapter bookAndChapter)
+	{
+		BookChapterPrevNext bc_pn;
+
+		if (bookAndChapter.BibleBook!.Value == BibleBook.Genesis.Value & bookAndChapter.Chapter == 1)
+		{
+			bc_pn = new(null, "", null); 
+			return bc_pn;
+		}
+
+		if (bookAndChapter.Chapter != 1)
+		{
+			var bookAndChapterPrev = bookAndChapter with
+			{
+				Chapter = bookAndChapter.Chapter - 1
+			};
+
+			bc_pn = new(bookAndChapterPrev, "fas fa-arrow-left", bookAndChapterPrev.Chapter.ToString());
+
+		}
+		else
+		{
+			var bookAndChapterPrev = bookAndChapter with
+			{
+				BibleBook = BibleBook.FromValue(bookAndChapter.BibleBook.Value - 1),
+				Chapter = bookAndChapter.Chapter - 1
+			};
+			bc_pn = new(bookAndChapterPrev, "fas fa-arrow-left", bookAndChapterPrev.Chapter.ToString());
+		}
+		return bc_pn;
+	}
+
+	private BookChapterPrevNext SetNextBookChapter(BookAndChapter bookAndChapter)
+	{
+		BookChapterPrevNext bc_pn;
+
+		if (bookAndChapter.BibleBook!.Value == VerseFacts.LastBookInNT &
+				bookAndChapter.Chapter == VerseFacts.LastChapterInNt)
+		{
+			bc_pn = new(null, "", null);
+		}
+
+		if (bookAndChapter.Chapter != bookAndChapter.BibleBook!.LastChapter)
+		{
+			var bookAndChapterPrev = bookAndChapter with
+			{
+				Chapter = bookAndChapter.Chapter + 1
+			};
+
+			bc_pn = new(bookAndChapterPrev, "fas fa-arrow-right", bookAndChapterPrev.Chapter.ToString());
+
+		}
+		else
+		{
+			var bookAndChapterPrev = bookAndChapter with
+			{
+				BibleBook = BibleBook.FromValue(bookAndChapter.BibleBook.Value + 1),
+				Chapter = 1
+			};
+			bc_pn = new(bookAndChapterPrev, "fas fa-arrow-right", bookAndChapterPrev.Chapter.ToString());
+		}
+
+		return bc_pn;
+
+	}
+
+}
---
+++ MyHebrewBible/MyHebrewBible.Client/Features/Typeahead/Person.cs
@@ -0,0 +1,22 @@
+﻿namespace MyHebrewBible.Client.Features.Typeahead;
+
+public class Person
+{
+	public Person() { }  // ctor?
+	public Person(int id, string firstname, string lastname, int age, string location)
+	{
+		Id = id;
+		Firstname = firstname;
+		Lastname = lastname;
+		Age = age;
+		Location = location;
+	}
+
+	public int Id { get; set; }
+	public string Firstname { get; set; }
+	public string Lastname { get; set; }
+	public string FullName { get => Firstname + " " + Lastname; }
+	public int Age { get; set; }
+	public string Location { get; set; }
+
+}
