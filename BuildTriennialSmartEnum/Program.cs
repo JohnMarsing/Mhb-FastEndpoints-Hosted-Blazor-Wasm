3720656581255a04d5fd9bf5668ee3b88f51539c
John Marsing <JohnMarsing@outlook.com>
2025-01-03 08:08:02 -0700

    Changed ProcessDatabase to rebuild all of Triennial.cs

--- BuildTriennialSmartEnum/Program.cs
+++ BuildTriennialSmartEnum/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Spectre.Console;
 using System.Data.SqlClient;
 using Dapper;
+using System.Text;
 
 internal class Program
 {
@@ -78,7 +79,7 @@ internal class Program
 		grid.AddRow(new Text[]{
 			new Text(stats.RowsRead.ToString()).Centered(),
 			new Text(stats.RowsWritten.ToString()).Centered(),
-			new Text(stats.FileName!).RightJustified()
+			new Text(stats.FinalFileName!).RightJustified()
 		});
 
 		AnsiConsole.WriteLine();
@@ -88,35 +89,91 @@ internal class Program
 
 	private static async Task ProcessDatabase(IProgress<ProgressInfo> progress, Stats stats)
 	{
-		string connectionString = "Data Source=JOHNSDELLDT\\SQLEXPRESS;Initial Catalog=Triennial;Integrated Security=True";
+		string connectionString = "Data Source=JOHNSDELLDT\\SQLEXPRESS;Initial Catalog=TriennialCodeGen;Integrated Security=True";
 
 		using (var connection = new SqlConnection(connectionString))
 		{
 			await connection.OpenAsync();  // connection.Open();
-			var sql = "SELECT Value, RegionIdCodeGen, RegionInstanceCodeGen, ClassCodeGen, TorahVerseCodeGen, HaftorahVerseCodeGen, BritVerseCodeGen FROM ParashaEnumTorah";
+			var sql = "SELECT Value, RegionIdCodeGen, RegionInstanceCodeGen, SealedClassTop, TorahVerseCodeGen, HaftorahVerseCodeGen, BritVerseCodeGen FROM TriennialCS";
 			var result = connection.Query<ParashaEnumTorah>(sql);
 
-			using (var writer = new StreamWriter(stats.FileName!)) //folder + "TriennialSmartEnum.txt"
-			{
-				await writer.WriteLineAsync("");
 
-				await writer.WriteLineAsync("#region Private Instantiation");
+			UTF8Encoding encodingWithBOM = new UTF8Encoding(true); // with a BOM (Byte Order Mark) i.e. saving "With signature"
+
+			/*
+			using (StreamWriter writer = new StreamWriter(fileName, false, encodingWithBOM))
+            
+			 */
+
+			using (var writer1 = new StreamWriter(Constants.Folder + Constants.TriennialCS_02_RegionId, false, encodingWithBOM))
+			{
+				await writer1.WriteLineAsync("	#region Id's");
+				await writer1.WriteLineAsync("	private static class Id");
+				await writer1.WriteLineAsync("	{");
+
+				foreach (var item in result)
+				{
+					stats.RowsRead++;
+					await writer1.WriteLineAsync($"{item.RegionIdCodeGen}");
+					stats.RowsWritten = stats.RowsWritten + 2; // counter++;
+					progress.Report(new ProgressInfo(.20));
+				}
+				await writer1.WriteLineAsync("	}");
+				await writer1.WriteLineAsync("#endregion");
+				await writer1.WriteLineAsync("");
+			}
+
+			using (var writer2 = new StreamWriter(Constants.Folder + Constants.TriennialCS_03_RegionInstance, false, encodingWithBOM))
+			{
+				await writer2.WriteLineAsync("#region Declared Public Instances");
+
+				foreach (var item in result)
+				{
+					stats.RowsRead++;
+					await writer2.WriteLineAsync($"{item.RegionInstanceCodeGen}");
+					stats.RowsWritten = stats.RowsWritten + 2; // counter++;
+					progress.Report(new ProgressInfo(.20));
+				}
+				await writer2.WriteLineAsync("");
+				await writer2.WriteLineAsync("#endregion");
+				await writer2.WriteLineAsync("");
+			}
+
+			using (var writer3 = new StreamWriter(Constants.Folder + Constants.TriennialCS_05_SealedClass, false, encodingWithBOM))
+			{
+				await writer3.WriteLineAsync("#region Private Instantiation");
 
 				foreach (var item in result)
 				{
 					stats.RowsRead++;
 					//await Task.Delay(50);
-					await writer.WriteLineAsync($"{item.ClassCodeGen}");
-					await writer.WriteLineAsync($"{item.TorahVerseCodeGen}");
-					await writer.WriteLineAsync($"{item.HaftorahVerseCodeGen}");
-					await writer.WriteLineAsync($"{item.BritVerseCodeGen} {Environment.NewLine} }}");  
+					await writer3.WriteLineAsync($"{item.SealedClassTop}");
+					await writer3.WriteLineAsync($"{item.TorahVerseCodeGen}");
+					await writer3.WriteLineAsync($"{item.HaftorahVerseCodeGen}");
+					await writer3.WriteLineAsync($"{item.BritVerseCodeGen} {Environment.NewLine} }}");
 					stats.RowsWritten = stats.RowsWritten + 4; // counter++;
 					progress.Report(new ProgressInfo(.64));
 				}
-				await writer.WriteLineAsync("");
-				await writer.WriteLineAsync("#endregion");
-
+				await writer3.WriteLineAsync("");
+				await writer3.WriteLineAsync("  #endregion");
+				await writer3.WriteLineAsync("");
+				await writer3.WriteLineAsync("}");
 			}
+
+			string? content;
+			content =
+				File.ReadAllText(Constants.Folder! + Constants.TriennialCS_01_Abstract_class) +
+				File.ReadAllText(Constants.Folder! + Constants.TriennialCS_02_RegionId) +
+				File.ReadAllText(Constants.Folder! + Constants.TriennialCS_03_RegionInstance) +
+				File.ReadAllText(Constants.Folder! + Constants.TriennialCS_04_Constructor_ExtraFields) +
+				File.ReadAllText(Constants.Folder! + Constants.TriennialCS_05_SealedClass);
+
+
+			using (StreamWriter writer = new StreamWriter(Constants.Folder + stats.FinalFileName!, false, encodingWithBOM))
+			{
+				writer.Write(content);
+			}
+
 		}
 	}
 }
@@ -127,16 +184,27 @@ public class ParashaEnumTorah
 	public int Value { get; set; }
 	public string? RegionIdCodeGen { get; set; }
 	public string? RegionInstanceCodeGen { get; set; }
-	public string? ClassCodeGen { get; set; }
+	public string? SealedClassTop { get; set; }
 	public string? TorahVerseCodeGen { get; set; }
 	public string? HaftorahVerseCodeGen { get; set; }
 	public string? BritVerseCodeGen { get; set; }
 }
 
+
+public static class Constants
+{
+	public const string? Folder = "C:\\Source\\repos\\FastEndpoints\\Mhb-FastEndpoints-Hosted-Blazor-Wasm-Backup\\055-Refactor-Triennial-to-TriennialCS\\";
+	public const string? TriennialCS_01_Abstract_class = "TriennialCS_01_Abstract_class.txt";
+	public const string? TriennialCS_02_RegionId = "TriennialCS_02_RegionId.txt";
+	public const string? TriennialCS_03_RegionInstance = "TriennialCS_03_RegionInstance.txt";
+	public const string? TriennialCS_04_Constructor_ExtraFields = "TriennialCS_04_Constructor_ExtraFields.txt";
+	public const string? TriennialCS_05_SealedClass = "TriennialCS_05_SealedClass.txt";
+}
+
+
 public class Stats
 {
 	public int RowsRead { get; set; } = 0;
 	public int RowsWritten { get; set; } = 0;
-	//public string? FileName { get; set; } = "C:\\Source\\repos\\FastEndpoints\\Mhb-FastEndpoints-Hosted-Blazor-Wasm-Backup\\034-CodeGen-Triennial-SmartEnum-class\\" + "TriennialSmartEnum.txt";
-	public string? FileName { get; set; } = "C:\\Source\\repos\\FastEndpoints\\Mhb-FastEndpoints-Hosted-Blazor-Wasm-Backup\\054-rebuild-TriennialSmartEnum\\Database-Changes-CodeGen\\" + "TriennialSmartEnum.txt";
+	public string? FinalFileName { get; set; } = "TriennialCS.txt";
 }
