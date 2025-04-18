698c9fd681a348945be66ba612ca0df64b4aaa92
John Marsing <JohnMarsing@outlook.com>
2025-04-18 12:59:16 -0700

    Renamed Cups, Moved VerseConent up one level

--- MyHebrewBible/MyHebrewBible.Client/Features/Haggadah/Enums/Content.cs
+++ MyHebrewBible/MyHebrewBible.Client/Features/Haggadah/Enums/Content.cs
@@ -13,16 +13,16 @@ public abstract class Content : SmartEnum<Content>
 		internal const int PassoverDeliverance = 3;
 		internal const int StoryOfRedemption = 4;
 		internal const int Matzah = 5;
-		internal const int FirstCup = 6;
-		internal const int SecondCup = 7;
-		internal const int ThirdCup = 8;
-		internal const int FourthCup = 9;
+		internal const int Cup1 = 6;
+		internal const int Cup2 = 7;
+		internal const int Cup3 = 8;
+		internal const int Cup4 = 9;
 		internal const int Charoset = 10;
 		internal const int Dinner = 11;
-		internal const int FifthCup = 12;
-		internal const int SixthCup = 13;
-		internal const int SeventhCup = 14;
-		internal const int NextYear = 15;
+		internal const int Cup5 = 12;
+		internal const int Cup6 = 13;
+		internal const int Cup7 = 14;
+		internal const int NextYear = 15; // This doesn't have a razor page FWIW
 		internal const int Appendix = 16;
 	}
 	#endregion
@@ -34,15 +34,15 @@ public abstract class Content : SmartEnum<Content>
 	public static readonly Content PassoverDeliverance = new PassoverDeliveranceSE();
 	public static readonly Content StoryOfRedemption = new StoryOfRedemptionSE();
 	public static readonly Content Matzah = new MatzahSE();
-	public static readonly Content FirstCup = new FirstCupSE();
-	public static readonly Content SecondCup = new SecondCupSE();
-	public static readonly Content ThirdCup = new ThirdCupSE();
-	public static readonly Content FourthCup = new FourthCupSE();
+	public static readonly Content Cup1 = new Cup1SE();
+	public static readonly Content Cup2 = new Cup2SE();
+	public static readonly Content Cup3 = new Cup3SE();
+	public static readonly Content Cup4 = new Cup4SE();
 	public static readonly Content Charoset = new CharosetSE();
 	public static readonly Content Dinner = new DinnerSE();
-	public static readonly Content FifthCup = new FifthCupSE();
-	public static readonly Content SixthCup = new SixthCupSE();
-	public static readonly Content SeventhCup = new SeventhCupSE();
+	public static readonly Content Cup5 = new Cup5SE();
+	public static readonly Content Cup6 = new Cup6SE();
+	public static readonly Content Cup7 = new Cup7SE();
 	public static readonly Content NextYear = new NextYearSE();
 	public static readonly Content Appendix = new AppendixSE();
 
@@ -64,7 +64,6 @@ public abstract class Content : SmartEnum<Content>
 	{
 		public override string Title => "Messiah, Savior, Redeemer";
 		public IntroductionSE() : base($"{nameof(Id.Introduction)}", Id.Introduction) { }
-		//public override int Section => 1;
 		public override MarkupString EngTitle => (MarkupString)(@"
 			<h2 class='text-center text-danger fw-bold mb-3'>
 				<span class='text-dark'>Messiah, Savior, Redeemer</span>
@@ -81,7 +80,6 @@ public abstract class Content : SmartEnum<Content>
 	{
 		public FirstPassoverSE() : base($"{nameof(Id.FirstPassover)}", Id.FirstPassover) { }
 		public override string Title => "1st Passover";
-		//public override int Section => 2;	
 		public override MarkupString EngTitle => (MarkupString)(@"
 			<h3 class='text-center'>
 				<span class='text-dark'>The First Passover</span>
@@ -142,9 +140,9 @@ public abstract class Content : SmartEnum<Content>
 			");
 	}
 
-	private sealed class FirstCupSE : Content
+	private sealed class Cup1SE : Content
 	{
-		public FirstCupSE() : base($"{nameof(Id.FirstCup)}", Id.FirstCup) { }
+		public Cup1SE() : base($"{nameof(Id.Cup1)}", Id.Cup1) { }
 		public override string Title => "1st Cup | Renewed Covenant";
 		public override MarkupString EngTitle => (MarkupString)(@"
 			<h2 class='text-center mb-1'>1<sup>st</sup> Cup of Ephraim’s Redemption</h2>
@@ -159,9 +157,9 @@ public abstract class Content : SmartEnum<Content>
 			");
 	}
 
-	private sealed class SecondCupSE : Content
+	private sealed class Cup2SE : Content
 	{
-		public SecondCupSE() : base($"{nameof(Id.SecondCup)}", Id.SecondCup) { }
+		public Cup2SE() : base($"{nameof(Id.Cup2)}", Id.Cup2) { }
 		public override string Title => "2nd Cup | Mercy";
 		public override MarkupString EngTitle => (MarkupString)(@"
 			<h2 class='text-center mb-1'>2<sup>nd</sup> Cup of Ephraim’s Redemption</h2>
@@ -176,9 +174,9 @@ public abstract class Content : SmartEnum<Content>
 			");
 	}
 
-	private sealed class ThirdCupSE : Content
+	private sealed class Cup3SE : Content
 	{
-		public ThirdCupSE() : base($"{nameof(Id.ThirdCup)}", Id.ThirdCup) { }
+		public Cup3SE() : base($"{nameof(Id.Cup3)}", Id.Cup3) { }
 		public override string Title => "3rd Cup | Identity";
 		public override MarkupString EngTitle => (MarkupString)(@"
 			<h2 class='text-center mb-1'>3<sup>rd</sup> Cup of Ephraim’s Redemption</h2>
@@ -193,9 +191,9 @@ public abstract class Content : SmartEnum<Content>
 			");
 	}
 
-	private sealed class FourthCupSE : Content
+	private sealed class Cup4SE : Content
 	{
-		public FourthCupSE() : base($"{nameof(Id.FourthCup)}", Id.FourthCup) { }
+		public Cup4SE() : base($"{nameof(Id.Cup4)}", Id.Cup4) { }
 		public override string Title => "4th Cup | Torah, Instructions";
 		public override MarkupString EngTitle => (MarkupString)(@"
 			<h2 class='text-center mb-1'>4<sup>th</sup> Cup of Ephraim’s Redemption</h2>
@@ -234,9 +232,9 @@ public abstract class Content : SmartEnum<Content>
 			");
 	}
 
-	private sealed class FifthCupSE : Content
+	private sealed class Cup5SE : Content
 	{
-		public FifthCupSE() : base($"{nameof(Id.FifthCup)}", Id.FifthCup) { }
+		public Cup5SE() : base($"{nameof(Id.Cup5)}", Id.Cup5) { }
 		public override string Title => "5th Cup | Holiness, Kadosh";
 		public override MarkupString EngTitle => (MarkupString)(@"
 			<h2 class='text-center mb-1'>5<sup>th</sup> Cup of Ephraim’s Redemption</h2>
@@ -251,9 +249,9 @@ public abstract class Content : SmartEnum<Content>
 			");
 	}
 
-	private sealed class SixthCupSE : Content
+	private sealed class Cup6SE : Content
 	{
-		public SixthCupSE() : base($"{nameof(Id.SixthCup)}", Id.SixthCup) { }
+		public Cup6SE() : base($"{nameof(Id.Cup6)}", Id.Cup6) { }
 		public override string Title => "6th Cup | Reconciliation";
 		public override MarkupString EngTitle => (MarkupString)(@"
 			<h2 class='text-center mb-1'>6<sup>th</sup> Cup of Ephraim’s Redemption</h2>
@@ -268,9 +266,9 @@ public abstract class Content : SmartEnum<Content>
 			");
 	}
 
-	private sealed class SeventhCupSE : Content
+	private sealed class Cup7SE : Content
 	{
-		public SeventhCupSE() : base($"{nameof(Id.SeventhCup)}", Id.SeventhCup) { }
+		public Cup7SE() : base($"{nameof(Id.Cup7)}", Id.Cup7) { }
 		public override string Title => "7th Cup | Return, Teshuva";
 		public override MarkupString EngTitle => (MarkupString)(@"
 			<h2 class='text-center mb-1'>7<sup>th</sup> Cup of Ephraim’s Redemption</h2>
