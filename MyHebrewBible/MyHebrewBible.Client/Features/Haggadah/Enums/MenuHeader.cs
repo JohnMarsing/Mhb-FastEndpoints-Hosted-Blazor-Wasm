12a9c92342e6f94b59fe1e604adf6d15b80415d5
John Marsing <JohnMarsing@outlook.com>
2025-05-07 18:03:19 -0700

    Refactor Haggadah, NavbarSkeleton, Progressbar, RightMenuSkeleton

---
+++ MyHebrewBible/MyHebrewBible.Client/Features/Haggadah/Enums/MenuHeader.cs
@@ -0,0 +1,59 @@
+﻿namespace MyHebrewBible.Client.Features.Haggadah.Enums;
+using Ardalis.SmartEnum;
+
+
+public abstract class MenuHeaders : SmartEnum<MenuHeaders>
+{
+	#region Id's
+	private static class Id
+	{
+		internal const int LanguageChoice = 1; // It's a feature to see this not only in Eng, but also in Esp, or both.  
+		internal const int ShowIntroductionSection = 2; // Who is the presenter, what the agenda, what Haggadah mean, 
+		internal const int ShowHaggadahOutlineModal = 3; // if shown, then show the progress bar
+		internal const int ShowPlaguesAppendix = 4;
+		internal const int ShowSummaryAppendix = 5; // What did you learn, key take aways, what about Easter?, what about the Threshold Covenant
+		internal const int ShowOriginalInstructionsAppendix = 6;
+		internal const int ShowBibleVerseAppendix = 7; // if shown, then list all verses used in the Haggadah
+	}
+	#endregion
+
+	#region Declared Public Instances
+	public static readonly MenuHeaders LanguageChoice = new LanguageChoiceSE();
+	public static readonly MenuHeaders ShowIntroductionSection = new ShowIntroductionSectionSE();
+	public static readonly MenuHeaders ShowHaggadahOutlineModal = new ShowHaggadahOutlineModalSE();
+	public static readonly MenuHeaders ShowBibleVerseAppendix = new ShowBibleVerseAppendixSE();
+	#endregion
+
+	private MenuHeaders(string name, int value) : base(name, value) { }
+
+	#region Extra Fields
+	public abstract string Header { get; }
+	#endregion
+
+	#region Private Instantiation
+
+	private sealed class LanguageChoiceSE : MenuHeaders
+	{
+		public LanguageChoiceSE() : base(nameof(LanguageChoice), Id.LanguageChoice) { }
+		public override string Header => "Language"; 
+	}
+
+	private sealed class ShowIntroductionSectionSE : MenuHeaders
+	{
+		public ShowIntroductionSectionSE() : base(nameof(ShowIntroductionSection), Id.ShowIntroductionSection) { }
+		public override string Header => "Introduction";
+	}
+
+	private sealed class ShowHaggadahOutlineModalSE : MenuHeaders
+	{
+		public ShowHaggadahOutlineModalSE() : base(nameof(ShowHaggadahOutlineModal), Id.ShowHaggadahOutlineModal) { }
+		public override string Header => "Haggadah Table of Contents";
+	}
+
+	private sealed class ShowBibleVerseAppendixSE : MenuHeaders
+	{
+		public ShowBibleVerseAppendixSE() : base(nameof(ShowBibleVerseAppendix), Id.ShowBibleVerseAppendix) { }
+		public override string Header => "Bible Verse Appendix";
+	}
+	#endregion
+}
---
+++ MyHebrewBible/MyHebrewBible.Client/Features/Haggadah/Enums/MenuItems.cs
@@ -0,0 +1,71 @@
+﻿namespace MyHebrewBible.Client.Features.Haggadah.Enums;
+
+using Ardalis.SmartEnum;
+
+public abstract class MenuItems : SmartEnum<MenuItems>
+{
+	#region Id's
+	private static class Id
+	{
+		internal const int English = 1;
+		internal const int Spanish = 2;
+		internal const int Both = 3;
+
+		internal const int ShowToC = 4;
+
+	}
+	#endregion
+
+	#region Declared Public Instances
+	public static readonly MenuItems English = new EnglishSE();
+	public static readonly MenuItems Spanish = new SpanishSE();
+	public static readonly MenuItems Both = new BothSE();
+	public static readonly MenuItems ShowToC = new ShowToCSE();
+
+	#endregion
+
+	private MenuItems(string name, int value) : base(name, value) { }
+
+	#region Extra Fields
+	public abstract int HeaderId { get; }
+	public abstract string Description { get; }
+	public abstract string Title { get; }
+	#endregion
+
+	#region Private Instantiation
+	private sealed class EnglishSE : MenuItems
+	{
+		public EnglishSE() : base(nameof(English), Id.English) { }
+		public override int HeaderId => MenuHeaders.LanguageChoice.Value;
+		public override string Title => "English";
+		public override string Description => "Display content in English only.";
+	}
+
+	private sealed class SpanishSE : MenuItems
+	{
+		public SpanishSE() : base(nameof(Spanish), Id.Spanish) { }
+		public override int HeaderId => MenuHeaders.LanguageChoice.Value;
+		public override string Title => "Español";
+		public override string Description => "Display content in Español only.";
+	}
+
+	private sealed class BothSE : MenuItems
+	{
+		public BothSE() : base(nameof(Both), Id.Both) { }
+		public override int HeaderId => MenuHeaders.LanguageChoice.Value;
+		public override string Title => "Eng + Esp";
+		public override string Description => "Display content in both English and Español.";
+	}
+
+	private sealed class ShowToCSE : MenuItems
+	{
+		public ShowToCSE() : base(nameof(ShowToC), Id.ShowToC) { }
+		public override int HeaderId => MenuHeaders.ShowHaggadahOutlineModal;
+		public override string Title => "Table of Content";
+		public override string Description => "Show T o C";
+	}
+
+	#endregion
+}
+
+/* public enum MenuItems {  English = 1, Spanish = 2,	Both = 3 } */
