2842bf87414ad982eb2fb1aa50b4f86fef06f134
John Marsing <JohnMarsing@outlook.com>
2025-05-18 14:32:35 -0700

    Finished EditModal ver1, add Font Helper

--- MyHebrewBible/MyHebrewBible.Client/Features/BookChapter/Enums/MenuItem.cs
+++ MyHebrewBible/MyHebrewBible.Client/Features/BookChapter/Enums/MenuItem.cs
@@ -8,13 +8,11 @@ public abstract class MenuItem : SmartEnum<MenuItem>
 	private static class Id
 	{
 		internal const int Instructions = 1;
-		internal const int ToggleSplitMode = 2;
 	}
 	#endregion
 
 	#region Declared Public Instances
 	public static readonly MenuItem Instructions = new InstructionsSE();
-	public static readonly MenuItem ToggleSplitMode = new ToggleSplitModeSE();
 	#endregion
 
 	private MenuItem(string name, int value) : base(name, value)  // Constructor
@@ -36,11 +34,5 @@ public abstract class MenuItem : SmartEnum<MenuItem>
 	{
 		public InstructionsSE() : base(nameof(Instructions), Id.Instructions) { }
 		public override string Title => "Show Instructions";
-	}
-
-	private sealed class ToggleSplitModeSE : MenuItem
-	{
-		public ToggleSplitModeSE() : base(nameof(ToggleSplitMode), Id.ToggleSplitMode) { }
-		public override string Title => "Toggle Split Mode";
 	}
 }
---
+++ MyHebrewBible/MyHebrewBible.Client/Helpers/Font.cs
@@ -0,0 +1,40 @@
+﻿namespace MyHebrewBible.Client.Helpers;
+
+
+/*
+```csharp
+@using MyHebrewBible.Client.Helpers
+ (userSetting.OnState) ? Icon.Toggle.On : Icon.Toggle.Off;
+```
+
+```html
+<i class='@Icon'></i> @Opposite
+```
+
+*/
+
+public static class Icon
+{
+	public static class Toggle
+	{
+		public const string On = " fas fa-toggle-on";
+		public const string Off = " fas fa-toggle-off";
+		public static string Get(bool isOn) => isOn ? Toggle.On : Toggle.Off;
+	}
+
+	public static class Chevron
+	{
+		public const string Down = " fas fa-chevron-down";
+		public const string Up = " fas fa-chevron-up";
+		public static string Get(bool isDown) => isDown ? Chevron.Down : Chevron.Up;
+	}
+
+
+	public static class Size
+	{
+		public const string OneX = "";
+		public const string TwoX = " fa-fw fa-2x";
+		public const string ThreeX = " fa-fw fa-3x";
+	}	
+
+}
